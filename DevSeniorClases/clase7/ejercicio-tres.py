e82055127fffaa95c1f39ec13e86e5dfc211c0eb
sandavidromero1-sys <sandavidromero1@gmail.com>
2025-11-03 21:21:22 -0500

    Actual

--- DevSeniorClases/clase7/ejercicio-tres.py
+++ DevSeniorClases/clase7/ejercicio-tres.py
@@ -21,22 +21,77 @@ While contador
 """
 menu de opciones por consola  
 """
-opcion = ""
+# opcion = ""
 
-print("-------Menu de opciones-------")
-print("1. Opcion 1: saludar")
-print("2. Opcion 2 : mensaje")
-print("3. Opcion 3: salir")
+# print("-------Menu de opciones-------")
+# print("1. Opcion 1: saludar")
+# print("2. Opcion 2 : mensaje")
+# print("3. Opcion 3: salir")
 
 
-while True:
-    opcion = input("Ingrese una opcion (1-3): ")
+# while True:
+#     opcion = input("Ingrese una opcion (1-3): ")
     
-    if opcion == "1":
-        print("Hola, bienvenido al programa")
-    elif opcion == "2":
-        print("Este es un mensaje importante")
-    elif opcion == "3":
-        print("saliendo de la aplicacion...")
-        break
+#     if opcion == "1":
+#         print("Hola, bienvenido al programa")
+#     elif opcion == "2":
+#         print("Este es un mensaje importante")
+#     elif opcion == "3":
+#         print("saliendo de la aplicacion...")
+#         break
     
+#Suma acumulada de numeros: Se solicita numeros al usuario y acumula su suma hasta que escriba 0.
+
+# suma = 0 
+# numero = int(input("Ingrese un numero (0 para terminar): "))
+
+# while numero != 0:
+#     suma += numero
+#     numero = int(input("Ingrese un numero (0 para terminar): "))
+
+# print(f"La suma acumulada es: {suma}")
+
+#validar la entrada de un numero positivo: solicitar un numero positivo y repetir hasta que se ingrese uno valido.
+
+# numero = int(input("Ingrese un numero positivo: "))
+# while numero < 0:
+#     print("Error: Debe ingresar un numero positivo.")
+#     numero = int(input("te solicito que ingreses un numero positivo: "))
+
+# print(f"Gracias! Ingreso el numero positivo: {numero}")
+
+# for se utiliza para recorrer una secuencia (lista, cadena, rango, etc)
+# imprimir numeros del 1 al 5
+# for numero in range(1, 6): # se imprime del 1 al 5
+#     print(f"Numero: {numero}")
+    
+#     #Ahora, quiero imprimir una lista de productos
+# productos = ["manzana", "banana", "naranja", "pera"]
+
+# for producto in productos:
+#     print(f"Producto: {producto}")
+    
+    #el uso del "break" y el "continue" en un bucle for
+    #brek: termina el bucle por completo
+    
+# for numero in range(1, 7):# se imprime del 1 al 6
+#     if numero == 4:
+#       print("Se interrumpio el ciclo for para excluir numeros mayores a 4")
+#       break
+#     print(f"El numero es: {numero}")
+
+# continue: salta a la siguiente iteracion del bucle
+
+# for numero in range(1, 7):# se imprime del 1 al 6
+#     if numero == 4:
+#         print("\n\n Por el uso de la PALABRA RESERVADA CONTINUE, se salta la impresion del numero 4\n\n ")
+#         continue
+#     print(f"El numero es: {numero}")
+    
+# for numero in range(1, 7, 2):# Se imprime del 1 al 6, pero de 2 en 2
+#     print(f"\nEl numero es: {numero}\n")
+
+caden = input ("Ingrese una cadena de texto: ")
+
+for caracter in caden:
+    print(f"Caracter: {caracter}")
