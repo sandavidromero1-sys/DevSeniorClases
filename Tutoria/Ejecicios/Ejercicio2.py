f5cfbee31f00cbe57f5cce6947f472fc30e98862
sandavidromero1-sys <sandavidromero1@gmail.com>
2025-11-08 21:21:08 -0500

    Ultima Tutoria3

---
+++ Tutoria/Ejecicios/Ejercicio2.py
@@ -0,0 +1,57 @@
+# palabra = str(input("Ingrese una palabra: "))
+
+# for  i in range(1,11):
+#  print(palabra)
+ 
+ 
+# i = 0
+# while  (i<=9):
+#     print(palabra)
+#     i+=1
+     
+
+# num = int(input("Ingrese un numero: "))
+
+# for i in range(1, num + 1):
+#     if i % 2 == 0 :
+#         print(f"El numero {i} es par")
+#     else:
+#         print(f"El numero {i} es impar")
+        
+# contrasena = ""
+# intentos = 0
+# while contrasena != "Radamel" and intentos < 2:
+#     validacion = str(input("Ingrese la contraseña de su correo (Tiene dos intentos): "))
+#     if validacion == "Radamel":
+#         print("Contraseña correcta, bienvenido")
+#         break
+#     else:
+#         print("Contraseña incorrecta, intente de nuevo")
+#         intentos+=1
+#         continue
+
+# print("Ha excedido el numero de intentos, su cuenta ha sido bloqueada")
+
+
+# for i in range(0,10,3):
+#     print(i)
+    
+# while True:
+#     print("hola mundo")
+#     num = int(input("Ingrese un numero (0 para salir): "))
+#     if num == 0:    
+#         break
+#     else:
+#         continue
+
+
+# num = int (input("Ingrese un numero positivo con la altura del triangulo: "))
+
+# for i in range (1,num + 1):
+#     line=""
+#     for i in range(1,num+1):
+#         line+="*"
+#         print(line)
+#     break
+        
+    
